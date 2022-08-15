1700ca63bf7271c66534719b8165140967c69f1e
maverickquill55 <VScode@toxik.cf>
2022-08-14 20:36:45 -0400

    Add logging to example.js

--- example.js
+++ example.js
@@ -13,4 +13,5 @@ console.log(`ROBOT #874, moving towards (${robot[1].finishLine.join(', ')})`)
 for(const state of interpret(...robot, true)) {
     // const [position, angle, finished, log] = state
     const [ position /* position the robot is at */, angle /* angle the robot is facing */, finished /* if the robot has reached the finish line */, log /* messages from interpreter, only shows if verbose is true */ ] = state
+    console.log(!finished?`\tRobot is at (${position.join(', ')}), facing ${angle} degrees`:"Robot reached finish line")
 }
